c9259ccd0cddaac96a0b970d5b773a7f6322f72c
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-10-31 00:21:53 -0500

    Adding styles to my component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// alternativa
-// const element = React.createElement('a', { href: 'https://www.platzi.com'}, 'Hola! Soy los children');
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
-// const name = 'Alice';
-// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
-
-// const jsx = <h1>Hola soy, {__expresiones__}</h1>;
-
-const element = (
-  <div>
-    <h1>Hola, soy Alice</h1>
-    <p>Soy Front-end Developer</p>
-  </div>
-);
-
-// const element = React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'Hola, soy Alice.'),
-//     React.createElement('p', {}, 'Soy Front-end Developer.')
-// );
+import Badge from "./components/Badge";
 
 const container = document.getElementById("app");
-
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
