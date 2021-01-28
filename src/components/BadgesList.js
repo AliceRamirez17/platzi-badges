4a15082135dc2ba3687a502f6c97af3df0c199a6
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2021-01-28 01:42:00 -0500

    Fetch API

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,25 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles/BadgesList.css";
 import twitterLogo from "../img/twitterLogo.png";
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
+    }
+
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
           return (
             <li key={badge.id} className="Badge__card">
               <div className="Badge__img">
-                <img className="Badge__avatar-list" src={badge.image} alt="" />
+                <img
+                  className="Badge__avatar-list"
+                  src={badge.avatarUrl}
+                  alt=""
+                />
               </div>
               <div className="Badge__info">
-                <h4 className="Badge__name">{badge.name}</h4>
+                <h4 className="Badge__name">{`${badge.firstName} ${badge.lastName}`}</h4>
                 <div className="Badge__username">
                   <img src={twitterLogo} alt="" />
-                  <p className="">@{badge.name}</p>
+                  <p className="">@{badge.twitter}</p>
                 </div>
-                <p>Specie: {badge.species}</p>
+                <p>Job Title: {badge.jobTitle}</p>
               </div>
             </li>
           );
